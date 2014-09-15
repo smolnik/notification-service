b6dbad24c192b98093d262e5195be0d8dd4ed063
smolnik <smolnik@wp.pl>
2014-09-15 10:48:51 +0200

    refactor: removing class ServiceNameResolver

--- src/main/java/net/adamsmolnik/setup/detection/WebSetup.java
+++ src/main/java/net/adamsmolnik/setup/detection/WebSetup.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebListener;
 import net.adamsmolnik.boundary.notification.NotificationActivityImpl;
 import net.adamsmolnik.control.notification.Notifier;
 import net.adamsmolnik.setup.ActivityLauncher;
-import net.adamsmolnik.setup.ServiceNameResolver;
 import net.adamsmolnik.util.Configuration;
 
 /**
@@ -16,9 +15,6 @@ import net.adamsmolnik.util.Configuration;
  */
 @WebListener("notificationActivitySetup")
 public class WebSetup implements ServletContextListener {
-
-    @Inject
-    private ServiceNameResolver snr;
 
     @Inject
     private Configuration conf;
@@ -31,7 +27,7 @@ public class WebSetup implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        notifier.subscribe(conf.getServiceValue(snr.getServiceName(), "sns.subscription.email"));
+        notifier.subscribe(conf.getServiceValue("sns.subscription.email"));
         al.register(new NotificationActivityImpl(notifier));
         al.launch();
     }
